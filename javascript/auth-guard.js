49fe654f0f19e802e5f05049855755bcfdc3c18a
Bitan56 <bitanchakraborty90@gmail.com>
2026-08-05 12:14:43 +0530

    add

--- javascript/auth-guard.js
+++ javascript/auth-guard.js
@@ -5,6 +5,7 @@
 
     // 2. If it does not exist, redirect to the index/login page
     if (!storedUserName) {
+        alert('You are log in first to use this feature.')
         window.location.replace('./logIn.html');
     }
     // If it does exist, the script does nothing and allows the page to load normally.
